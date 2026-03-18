1e52829df5db49ff4f966245ae5602e279de1280
jinxinglim <jinxing_1990@hotmail.com>
2026-03-18 09:22:37 +0000

    fix: use correct testUSDC token ID in tests

---
+++ test-deposit-arb2.mjs
@@ -0,0 +1,42 @@
+import { AllSetProvider, createEvmExecutor, createEvmWallet } from './dist/index.js';
+
+const FAST_ADDRESS = 'fast15cqs95q832e63a2ypcelvuzev7g5x8t0vc4kdyquumwdhvw0ykdsmjz48q';
+const EVM_KEYFILE = process.env.HOME + '/.evm/keys/test-wallet.json';
+
+async function main() {
+  console.log('Setting up...');
+  
+  const allset = new AllSetProvider({ network: 'testnet' });
+  const account = createEvmWallet(EVM_KEYFILE);
+  
+  console.log('EVM Address:', account.address);
+  console.log('Fast Address:', FAST_ADDRESS);
+  
+  // Arbitrum Sepolia RPC
+  const rpcUrl = 'https://sepolia-rollup.arbitrum.io/rpc';
+  const chainId = 421614;
+  
+  const evmClients = createEvmExecutor(account, rpcUrl, chainId);
+  
+  console.log('\nDepositing 0.000001 USDC from Arbitrum Sepolia to Fast...');
+  
+  const result = await allset.sendToFast({
+    chain: 'arbitrum',
+    token: 'USDC',
+    amount: '1', // 0.000001 USDC (6 decimals)
+    from: account.address,
+    to: FAST_ADDRESS,
+    evmClients,
+  });
+  
+  console.log('\nSuccess!');
+  console.log('TX Hash:', result.txHash);
+  console.log('Order ID:', result.orderId);
+  console.log('Estimated Time:', result.estimatedTime);
+}
+
+main().catch(err => {
+  console.error('\nError:', err.message);
+  if (err.code) console.error('Code:', err.code);
+  if (err.context) console.error('Context:', JSON.stringify(err.context, null, 2));
+});
